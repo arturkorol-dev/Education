a855c99c8dffbde771fe73e7211e6a1f5b192468
Artur Korol <korolarthur7@gmail.com>
2022-08-14 13:49:43 +0300

    Create project

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Artur Korol on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
