271cb730a0f37a5cb9ff93f9d364f4d9c01336f7
Artur Korol <korolarthur7@gmail.com>
2022-08-18 19:22:16 +0300

    Create DevoteApp

---
+++ Devote/Devote/App/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Artur Korol on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
