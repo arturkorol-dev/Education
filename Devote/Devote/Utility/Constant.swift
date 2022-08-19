ae00677fa15070e3d2761351739e714f28765479
Artur Korol <korolarthur7@gmail.com>
2022-08-19 11:45:06 +0300

    Code refactor

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Artur Korol on 19.08.2022.
+//
+
+import SwiftUI
+
+//MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
