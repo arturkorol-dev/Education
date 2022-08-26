a840481344a71c10c5bf59c2a9cba00da4d3fc38
Artur Korol <korolarthur7@gmail.com>
2022-08-26 09:32:35 +0300

    Add preferred color scheme for the app

--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -10,6 +10,7 @@ import CoreData
 
 struct ContentView: View {
     //MARK: - Property
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task = ""
     @State private var showNewItemTask = false
     
@@ -43,6 +44,37 @@ struct ContentView: View {
             ZStack {
                 VStack {
                     //MARK: - Header
+                    HStack(spacing: 10) {
+                        // Title
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                        Spacer()
+                        
+                        // Edit button
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule()
+                                    .stroke(Color.white, lineWidth: 2)
+                            )
+                        // Appearance button
+                        Button {
+                            isDarkMode.toggle()
+                        } label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        }
+
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    
                     Spacer(minLength: 80)
                     
                     //MARK: - NewTaskButton
@@ -107,12 +139,7 @@ struct ContentView: View {
             .onAppear(perform: {
                 UITableView.appearance().backgroundColor = .clear
             })
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            }
-            .navigationTitle("Daily tasks")
+            .navigationBarHidden(true)
         }
         .navigationViewStyle(.stack)
     }
