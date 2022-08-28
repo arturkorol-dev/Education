c979e5045efde0f8c0f43beac38333c6773ce03a
Artur Korol <korolarthur7@gmail.com>
2022-08-28 17:35:52 +0300

    Create project, add images and accent color.

---
+++ Notes/Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Artur Korol on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
