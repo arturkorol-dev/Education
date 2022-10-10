259b4550924a7cdd879bd67837bba184e0f02499
Artur Korol <korolarthur7@gmail.com>
2022-10-10 19:53:50 +0300

    GitHubFollowersApp - Add custom reuzable button

---
+++ GitHubFollowers/GitHubFollowers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Artur Korol on 10.10.2022.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    //MARK: - Initialisation
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(title: String, backgroundColor: UIColor) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.backgroundColor = backgroundColor
+        configure()
+    }
+    
+    //MARK: - Private method
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
