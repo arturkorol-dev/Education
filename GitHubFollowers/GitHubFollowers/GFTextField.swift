6dbd24d860a8e274e2c389e97a694ed3bfb0f581
Artur Korol <korolarthur7@gmail.com>
2022-10-11 07:45:26 +0300

    GitHubFollowersApp - Add custom reuzable TextField

---
+++ GitHubFollowers/GitHubFollowers/GFTextField.swift
@@ -0,0 +1,39 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Artur Korol on 11.10.2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    //MARK: - Initialisation
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    //MARK: - Private method
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter user name"
+    }
+}
